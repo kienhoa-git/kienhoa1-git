66f7665bb38cac4a6d28bd7446fb39b9f547dfdd
kienhoa <giaumavip@gmail.com>
2024-01-18 17:48:02 +0700

    update code 1

--- math.js
+++ math.js
@@ -223,5 +223,85 @@ callHell(1)
       var html = htmls.join('')
       document.querySelector('.block').innerHTML = html
      });  
+// kiến thức code nâng cao của fecth và hoạt động json trong cơ sở dữ liệu
+var listCoursesBlock = document.querySelector('.courses-list')
+var courseApi = 'http://localhost:3000/courses'
+function start() {
+       getCourses(renderCourses)
+       handleCreateForm()
+}
+start();
+// Functions
+function getCourses(callback) {
+  fetch(courseApi)
+      .then(response => {return response.json()})
+      .then(callback)
+}
+// hoạt động createCourses : data dữ liệu gửi đi đc chuyển từ js sang json sau đó mình nhận call back của js
+function createCourses(data, callback) {
+  var options = {
+           method: 'POST',
+           body: JSON.stringify(data),
+           headers: {
+            'Content-Type' : 'application/json'
+           }
+  }
+  fetch(courseApi,options)
+       .then(response =>{return response.json()})
+       .then(callback)
+}
+function handleDeleteCourses(id) {
+  var options = {
+    method: 'DELETE',
+    headers: {
+     'Content-Type' : 'application/json'
+    }
+}
+fetch(courseApi +'/'+ id,options)
+.then(response =>{return response.json()})
+.then(()=>{
+  var courseItem = document.querySelector('.courses-item-'+ id)
+  if(courseItem) {
+    courseItem.remove()
+  }
+})
+}
+function update(data) {
+  var options = {
+    method: 'PUT',
+    body: JSON.stringify(data),
+    headers: {
+     'Content-Type' : 'application/json'
+    }
+}
+fetch(courseApi +'/'+ id,options)
+.then(response =>{return response.json()})
+.then(()=>{
+  getCourses(renderCourses)
+})
   
-  
+}
+function renderCourses(courses) {
+    var htmls = courses.map(course =>{
+      return `<li class="courses-item-${course.id}">
+             <h4>${course.name}</h4>
+             <p>${course.description}</p>
+             <button onclick="handleDeleteCourses(${course.id})">Xóa</button>
+      </li>`
+    })
+    listCoursesBlock.innerHTML = htmls.join('')
+}
+function handleCreateForm() {
+      var createBtn = document.querySelector('#Create')
+      createBtn.onclick = function() {
+        var name = document.querySelector('input[name="name"]').value
+        var Description = document.querySelector('input[name="description"]').value
+        var formData = {
+          name: name,
+          description: Description
+        }
+        createCourses(formData,()=>{
+          getCourses(renderCourses)
+        })
+      }
+}  
