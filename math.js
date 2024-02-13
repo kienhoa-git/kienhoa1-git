d34454875e2a5cbfb7aedfc52503e485c2b934f1
kienhoa <giaumavip@gmail.com>
2024-02-13 15:54:35 +0700

    changed code

--- math.js
+++ math.js
@@ -331,3 +331,72 @@ function hignlight([first, ...rest], ...strings) {
 var brand = "f8";
 var course = "Javascript";
 hignlight`học lặp trình ${course} tại ${brand}!`;
+// call API
+var data = {
+  users: [
+    {
+      id: 1,
+      name: "neko",
+    },
+    {
+      id: 2,
+      name: "hentai",
+    },
+    {
+      id: 3,
+      name: "z",
+    },
+  ],
+  comments: [
+    {
+      id: 1,
+      user_id: 1,
+      content: "chưa ra viddeo =((",
+    },
+    {
+      id: 2,
+      user_id: 2,
+      content: "đợi tí",
+    },
+  ],
+};
+function getComment() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(comment);
+    }, 1000);
+  });
+}
+function getUserById(userIds) {
+  return new Promise((resolve) => {
+    var user = users.filter((user) => {
+      return userIds.includes(user.id);
+    });
+    setTimeout(() => {
+      resolve(user);
+    }, 1000);
+  });
+}
+getComment()
+  .then((comments) => {
+    var userIds = comments.map((user) => {
+      return user.user_id;
+    });
+    return getUserById(userIds).then((users) => {
+      return {
+        users: users,
+        comments: comments,
+      };
+    });
+  })
+  .then((data) => {
+    var commentBlock = document.querySelector(".courses-list");
+    var html = "";
+    data.comments.forEach((comment) => {
+      var player = data.users.find((user) => {
+        return user.id === comment.id;
+      });
+    });
+    html += `<li>${userId.name}: ${comment.content}</li>`;
+    commentBlock.innerHTML = html;
+  });
